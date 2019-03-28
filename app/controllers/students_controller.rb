eac64a9f88a9e0a87eabad16a3d0ded3f0d61b75
Ashley Wong <ashwong04@gmail.com>
2019-03-28 16:55:43 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,8 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to student
+    redirect_to '/student/:id'
+  
   end
 
 end
